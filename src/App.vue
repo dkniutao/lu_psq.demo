76d732e785f273b2cfff83457989475df1cb18e6
扬神233 <young129@126.com>
2018-04-26 16:25:01 +0800

    初始化页面头部

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container>
+      <el-header>
+        <el-row>
+          <el-col :span="12">
+            <div class="fl logo">
+              <i class="fl iconfont icon-logo"></i>
+              <span class="logo-title">山东教科院教育评估管理平台</span> | <span class="logo-subtitle">问卷调查系统</span>
+            </div>
+          </el-col>
+          <el-col :span="12">
+            <div class="fr"><span>管理员001</span></div>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
@@ -12,12 +28,11 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+#app{height: 100%;}
+.el-header {background: #0e1b38;color: #fff;line-height: 60px;}
+.logo .iconfont{font-size: 30px;}
+.logo .logo-title{font-size: 18px; font-weight: bold;margin-left: 6px;}
+.logo .logo-subtitle{font-size: 16px;}
+
+.el-container{background: #eee;height: 100%;}
 </style>
