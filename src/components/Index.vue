a42733c233a94df885fdfaf1dda11418a4bc87b2
扬神233 <young129@126.com>
2018-04-27 17:03:53 +0800

    处理列表排序和筛选值

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -16,16 +16,16 @@
     <div class="psq-table">
       <el-table
         :data="tableData"
+        @filter-change="filterChange"
+        @sort-change="sortChange"
         style="width: 100%">
         <el-table-column
           prop="id"
-          label="排序"
-        >
+          label="排序">
         </el-table-column>
         <el-table-column
           prop="name"
-          label="标题"
-        >
+          label="标题">
         </el-table-column>
         <el-table-column
           prop="create_time"
@@ -34,15 +34,20 @@
         </el-table-column>
         <el-table-column
           :filters="[
-            { text: '未发布', value: '未发布' },
-            { text: '进行中', value: '进行中' },
-            { text: '已暂停', value: '已暂停' },
-            { text: '已结束', value: '已结束' }
+            { text: '未发布', value: 0 },
+            { text: '进行中', value: 1 },
+            { text: '已暂停', value: 2 },
+            { text: '已结束', value: 3 }
           ]"
-          :filter-method="filterStatus"
+          column-key="status"
           prop="status"
-          label="状态"
-        >
+          label="状态">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.status == 0" type="info">未发布</el-tag>
+            <el-tag v-if="scope.row.status == 1" type="success">进行中</el-tag>
+            <el-tag v-if="scope.row.status == 2" type="danger">已暂停</el-tag>
+            <el-tag v-if="scope.row.status == 3" type="warning">已结束</el-tag>
+          </template>
         </el-table-column>
         <el-table-column
           prop="answer_count"
@@ -50,7 +55,13 @@
         </el-table-column>
         <el-table-column
           prop="operate"
+          width="400"
           label="操作">
+          <template slot-scope="scope">
+            <el-button type="text"><i class="iconfont icon-fabu"></i>发布</el-button>
+            <el-button type="text"><i class="iconfont icon-zanting"></i>暂停</el-button>
+            <el-button type="text"><i class="iconfont icon-tongji"></i>统计</el-button>
+          </template>
         </el-table-column>
       </el-table>
       <el-pagination
@@ -75,27 +86,46 @@ export default {
       currentPage: 1,
       pageSize: 20,
       total: 0,
+      create_time: '',
+      status: [],
       tableData: []
     }
   },
   methods: {
+    renderTable () {
+      mylib.axios({
+        url: 'questionnaire/list',
+        params: {
+          keyword: '',
+          page: this.currentPage,
+          status: this.status.length ? this.status.join(',') : '',
+          sort: this.create_time ? 'create_time ' + this.create_time : ''
+        },
+        done (res) {
+          this.tableData = res.data.items
+          this.currentPage = +res.data._meta.currentPage
+          this.pageSize = +res.data._meta.perPage
+          this.total = +res.data._meta.totalCount
+        }
+      }, this)
+    },
+    resetTable () {
+      this.currentPage = 1
+      this.renderTable()
+    },
     handleSizeChange () {},
     handleCurrentChange () {},
-    filterStatus () {}
+    filterChange (filters) {
+      this.status = filters.status
+      this.resetTable()
+    },
+    sortChange (sort) {
+      this.create_time = sort.order ? sort.order.replace(/ending/ig, '') : ''
+      this.resetTable()
+    }
   },
   mounted () {
-    mylib.axios({
-      url: 'questionnaire/list',
-      params: {
-        keyword: ''
-      },
-      done (res) {
-        this.tableData = res.data.items
-        this.currentPage = +res.data._meta.currentPage
-        this.pageSize = +res.data._meta.perPage
-        this.total = +res.data._meta.totalCount
-      }
-    }, this)
+    this.renderTable()
   }
 }
 </script>
