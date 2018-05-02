4dffad40a7d877b7eed36625f8b4cf597cedc358
扬神233 <young129@126.com>
2018-05-02 16:21:16 +0800

    处理单选编辑功能

--- src/components/xz/questionItemEditRadio.vue
+++ src/components/xz/questionItemEditRadio.vue
@@ -2,16 +2,16 @@
 <div>
   <el-row>
     <el-col :span="12">
-      <quill-editor v-model="source.title" ref="myQuillEditor" :options="editorOption">
+      <quill-editor v-model="source.title" ref="myQuillEditor">
       </quill-editor>
     </el-col>
     <el-col :span="12">
     </el-col>
     <el-col :span="24">
-      <el-table :data="tableData" style="width: 100%">
+      <el-table :data="source.options" style="width: 100%">
         <el-table-column prop="text" label="选项文字">
           <template slot-scope="scope">
-            <el-input v-model="scope.row.text" placeholder="请输入选项内容"></el-input>
+            <el-input v-model="scope.row.title" placeholder="请输入选项内容"></el-input>
           </template>
         </el-table-column>
         <el-table-column prop="img" label="图片" width="180">
@@ -21,10 +21,10 @@
         </el-table-column>
         <el-table-column prop="operate" label="操作">
           <template slot-scope="scope">
-            <el-button type="text" style="float:left;"><i style="font-size:20px;color:#126ab5" class="iconfont icon-jia1"></i></el-button>
-            <el-button type="text" style="float:left;"><i style="font-size:20px;color:#126ab5" class="iconfont icon-jian1"></i></el-button>
-            <el-button type="text" style="float:left;"><i style="font-size:24px;color:#126ab5" class="iconfont icon-xiayi"></i></el-button>
-            <el-button type="text" style="float:left;"><i style="font-size:24px;color:#126ab5" class="iconfont icon-shangyi"></i></el-button>
+            <el-button class="fl" type="text" @click="add(scope)"><i class="iconfont icon-jia1"></i></el-button>
+            <el-button class="fl" type="text" @click="del(scope)"><i class="iconfont icon-jian1"></i></el-button>
+            <el-button class="fl" type="text" @click="down(scope)"><i class="iconfont icon-xiayi"></i></el-button>
+            <el-button class="fl" type="text" @click="up(scope)"><i class="iconfont icon-shangyi"></i></el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -36,6 +36,21 @@
 <script>
 export default {
   props: ['source'],
-  data () {}
+  data () {return {}},
+  methods: {
+    add (scope) {
+      this.source.options.splice(scope.$index + 1, 0, {
+        id: '1',
+        title: scope.$index + 1
+      });
+    },
+    del (scope) {
+      this.source.options.splice(scope.$index, 1);
+    }
+  }
 }
 </script>
+
+<style scoped>
+.el-table .el-button .iconfont{font-size:20px;color:#126ab5}
+</style>
