383f7f025e4ee921e0634cd6f3e316c6b9a2ce4d
扬神233 <young129@126.com>
2018-05-03 14:55:28 +0800

    调整样式

--- src/components/edit/QItem.vue
+++ src/components/edit/QItem.vue
@@ -15,14 +15,30 @@
 
     <div class="item-operate clearfix">
       <el-button v-show="order != '0'" class="fl operate-btn">在此题后插入新题</el-button>
-      <el-button class="fr operate-btn"><i class="iconfont icon-zhidingdel"></i>最后</el-button>
-      <el-button class="fr operate-btn"><i class="iconfont icon-icon_zhiding"></i>最前</el-button>
-      <el-button class="fr operate-btn"><i class="iconfont icon-xiayi"></i>下移</el-button>
-      <el-button class="fr operate-btn"><i class="iconfont icon-shangyi"></i>上移</el-button>
-      <el-button class="fr operate-btn"><i class="iconfont icon-iconless"></i>删除</el-button>
-      <el-button class="fr operate-btn"><i class="iconfont icon-fuzhi"></i>复制</el-button>
-      <el-button v-show="!isExpand" class="fr operate-btn" @click="edit"><i class="iconfont icon-shuru"></i>编辑</el-button>
-      <el-button v-show="isExpand" class="fr operate-btn" @click="complete"><i class="iconfont icon-wancheng"></i>完成</el-button>
+      <el-button class="fr operate-btn">
+        <i class="iconfont icon-zhidingdel"></i>最后
+      </el-button>
+      <el-button class="fr operate-btn">
+        <i class="iconfont icon-icon_zhiding"></i>最前
+      </el-button>
+      <el-button class="fr operate-btn">
+        <i class="iconfont icon-xiayi"></i>下移
+      </el-button>
+      <el-button class="fr operate-btn">
+        <i class="iconfont icon-shangyi"></i>上移
+      </el-button>
+      <el-button class="fr operate-btn">
+        <i class="iconfont icon-iconless"></i>删除
+      </el-button>
+      <el-button class="fr operate-btn">
+        <i class="iconfont icon-fuzhi"></i>复制
+      </el-button>
+      <el-button v-show="!isExpand" class="fr operate-btn" @click="edit">
+        <i class="iconfont icon-shuru"></i>编辑
+      </el-button>
+      <el-button v-show="isExpand" class="fr operate-btn" @click="complete">
+        <i class="iconfont icon-wancheng"></i>完成
+      </el-button>
     </div>
 
     <div class="item-edit">
@@ -92,12 +108,16 @@ export default {
 <style>
 /*基础 start*/
 .question-item{box-sizing: border-box;border: 1px solid #fff;margin: 0 60px 20px;padding:20px;}
+.question-item .item-header{padding-bottom:20px;margin-bottom:20px;border-bottom:1px solid #f3f3f3;}
+.question-item .item-operate{padding-bottom:20px;margin-bottom:20px;border-bottom:1px solid #f3f3f3;}
+.question-item .item-edit{margin-bottom: 20px;}
 /*基础 end*/
 
 /*悬浮 start*/
 .question-item:hover{border-color:#126ab5;cursor: pointer;}
 .question-item .item-operate,.question-item .item-edit,.question-item .item-submit{display: none;}
 .question-item:hover .item-operate{display: block;}
+.question-item .item-submit{text-align: center;}
 /*悬浮 end*/
 
 /*展开 start*/
@@ -105,19 +125,15 @@ export default {
 .question-item.expand .item-operate,.question-item.expand .item-edit,.question-item.expand .item-submit{display: block;}
 /*展开 end*/
 
-.question-item .item-header{padding-bottom:20px;margin-bottom:20px;border-bottom:1px solid #f3f3f3;}
-.question-item .item-operate{padding-bottom:20px;margin-bottom:20px;border-bottom:1px solid #f3f3f3;}
-.question-item .item-edit{margin-bottom: 20px;}
-
-.title-input{width:100%;color:#333;font-size: 14px;line-height: 30px;border:none;outline: none;font-weight: bold;}
-
+/*操作按钮 start*/
 .operate-btn.el-button{border:1px solid #126ab5;border-radius: 0;color: #126ab5;padding:0 12px;font-size: 12px;height: 22px;line-height: 22px;margin-left: 10px;}
 .operate-btn.el-button:hover{background:#d0e1f0;}
 .operate-btn.el-button .iconfont{float:left;}
+/*操作按钮 end*/
 
-.item-submit{text-align: center;}
-
+/*提交按钮 start*/
 .question-submit.el-button--primary, .question-submit.el-button--primary.is-active, .question-submit.el-button--primary:active {background: #126ab5;border-color: #126ab5;}
 .question-submit.el-button--primary:focus, .question-submit.el-button--primary:hover {background: #4188c4;border-color: #4188c4;}
 .question-submit.el-button {border-radius: 0;padding: 7px 24px;}
+/*提交按钮 end*/
 </style>
