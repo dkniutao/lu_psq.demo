896531cc48013ac8931baaea04c4888aac87cedf
扬神233 <young129@126.com>
2018-05-04 17:40:33 +0800

    处理多选评分

---
+++ src/components/edit/QItemShowRateMulti.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="clearfix">
+  <el-table
+    :data="item.setting"
+    border
+    style="width: 100%">
+    <el-table-column
+      prop="title"
+      label=""
+      width="180">
+    </el-table-column>
+    <el-table-column
+      v-for="column in item.content"
+      :label="column.title + '(分值:' + column.score + ')'">
+      <template slot-scope="scope">
+        <el-radio :label="column.score">&nbsp;</el-radio>
+      </template>
+    </el-table-column>
+  </el-table>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['item']
+}
+</script>
