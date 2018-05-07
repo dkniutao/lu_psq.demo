9dd926ae69d6fcd446a02da0efccb8aba793462b
扬神233 <young129@126.com>
2018-05-07 13:55:51 +0800

    修改代码格式

--- src/components/edit/QItemEditSelect.vue
+++ src/components/edit/QItemEditSelect.vue
@@ -5,12 +5,17 @@
       <el-table :data="item.content" style="width: 100%">
         <el-table-column prop="text" label="选项文字">
           <template slot-scope="scope">
-            <el-input v-model="scope.row.title" placeholder="请输入选项文字"></el-input>
+            <el-input 
+              v-model="scope.row.title" 
+              placeholder="请输入选项文字">
+            </el-input>
           </template>
         </el-table-column>
         <el-table-column prop="img" label="图片" width="240">
           <template slot-scope="scope">
-            <el-button type="text" @click="uploadImg(scope.row, scope.$index)">
+            <el-button 
+              type="text" 
+              @click="uploadImg(scope.row, scope.$index)">
               <i class="iconfont icon-tupian2" style="font-size: 25px;"></i>
               <span v-text="getImgName(scope.row.img)"></span>
             </el-button>
@@ -18,16 +23,28 @@
         </el-table-column>
         <el-table-column prop="operate" label="操作">
           <template slot-scope="scope">
-            <el-button class="fl" type="text" @click="add(scope.row, scope.$index)">
+            <el-button 
+              class="fl" 
+              type="text" 
+              @click="add(scope.row, scope.$index)">
               <i class="iconfont icon-jia1"></i>
             </el-button>
-            <el-button class="fl" type="text" @click="del(scope.row, scope.$index)">
+            <el-button 
+              class="fl" 
+              type="text" 
+              @click="del(scope.row, scope.$index)">
               <i class="iconfont icon-jian1"></i>
             </el-button>
-            <el-button class="fl" type="text" @click="down(scope.row, scope.$index)">
+            <el-button 
+              class="fl" 
+              type="text" 
+              @click="down(scope.row, scope.$index)">
               <i class="iconfont icon-xiayi" style="font-size: 25px;"></i>
             </el-button>
-            <el-button class="fl" type="text" @click="up(scope.row, scope.$index)">
+            <el-button 
+              class="fl" 
+              type="text" 
+              @click="up(scope.row, scope.$index)">
               <i class="iconfont icon-shangyi" style="font-size: 25px;"></i>
             </el-button>
           </template>
@@ -35,7 +52,10 @@
       </el-table>
     </el-col>
   </el-row>
-  <xz-upload-img :row="uploadImgRow" :visible.sync="uploadImgVisible"></xz-upload-img>
+  <xz-upload-img 
+    :row="uploadImgRow" 
+    :visible.sync="uploadImgVisible">
+  </xz-upload-img>
 </div>
 </template>
 
