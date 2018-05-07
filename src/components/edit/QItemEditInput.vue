b45e7fd8e272d1f640d122930c73d4fb8447dfc5
扬神233 <young129@126.com>
2018-05-07 13:58:25 +0800

    修改代码格式

--- src/components/edit/QItemEditInput.vue
+++ src/components/edit/QItemEditInput.vue
@@ -18,10 +18,22 @@
           </el-select>
         </el-form-item>
         <el-form-item label="最小字数">
-          <el-input-number v-model="item.setting.min" :controls="false" @blur="checkNumber('min')" :min="0" :max="99999"></el-input-number>
+          <el-input-number 
+            v-model="item.setting.min" 
+            :controls="false" 
+            :min="0" 
+            :max="99999"
+            @blur="checkNumber('min')">
+          </el-input-number>
         </el-form-item>
          <el-form-item label="最大字数">
-          <el-input-number v-model="item.setting.max" :controls="false" @blur="checkNumber('max')" :min="0" :max="99999"></el-input-number>
+          <el-input-number 
+            v-model="item.setting.max" 
+            :controls="false" 
+            :min="0" 
+            :max="99999"
+            @blur="checkNumber('max')">
+          </el-input-number>
         </el-form-item>
       </el-form>
     </el-col>
