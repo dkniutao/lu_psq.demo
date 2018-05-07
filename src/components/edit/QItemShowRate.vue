f3e806963117c3a77f97517e77112fcc66fab8ae
扬神233 <young129@126.com>
2018-05-07 15:44:03 +0800

    优化代码格式

--- src/components/edit/QItemShowRate.vue
+++ src/components/edit/QItemShowRate.vue
@@ -2,7 +2,12 @@
 <div class="clearfix">
   <span class="fl">{{item.content[0]['title']}}</span>
   <el-radio-group class="fl">
-    <el-radio v-for="option in item.content" :label="option.score" :title="option.title">{{option.score}}</el-radio>
+    <el-radio 
+      v-for="option in item.content" 
+      :label="option.score" 
+      :title="option.title">
+      {{option.score}}
+    </el-radio>
   </el-radio-group>
   <span class="fl">{{item.content[item.content.length - 1]['title']}}</span>
 </div>
