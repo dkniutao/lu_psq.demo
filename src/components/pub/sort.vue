874c361838443efab969fc31213cce163ba9b490
扬神233 <young129@126.com>
2018-05-10 12:09:26 +0800

    处理排序题

--- src/components/pub/sort.vue
+++ src/components/pub/sort.vue
@@ -6,7 +6,7 @@
     :key="sort.key"
     @click="setSort(sort)">
       <span aria-checked="mixed" class="xz-sort__input">
-        <span class="xz-sort__inner"></span>
+        <span class="xz-sort__inner">12</span>
       </span>
 
       <span class="xz-sort__label">
@@ -28,8 +28,11 @@ export default {
 </script>
 <style scoped>
 .xz-sort {color: #333333;font-size: 14px;cursor: pointer;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;padding-top: 20px;}
+
 .xz-sort__input{cursor: pointer;outline: 0;line-height: 1;vertical-align: middle;}
 .xz-sort__input {display: inline-block;position: relative;white-space: nowrap;}
-.xz-sort__inner{display: inline-block;position: relative;border: 1px solid #126ab5;-webkit-box-sizing: border-box;box-sizing: border-box;width: 14px;height: 14px;background-color: #fff;z-index: 1;-webkit-transition: border-color .25s cubic-bezier(.71,-.46,.29,1.46),background-color .25s cubic-bezier(.71,-.46,.29,1.46);transition: border-color .25s cubic-bezier(.71,-.46,.29,1.46),background-color .25s cubic-bezier(.71,-.46,.29,1.46);}
+
+.xz-sort__inner{font-size:12px;color:#126ab5;line-height:16px;text-align:center;display: inline-block;position: relative;border: 1px solid #126ab5;-webkit-box-sizing: border-box;box-sizing: border-box;width: 18px;height: 18px;background-color: #fff;z-index: 1;-webkit-transition: border-color .25s cubic-bezier(.71,-.46,.29,1.46),background-color .25s cubic-bezier(.71,-.46,.29,1.46);transition: border-color .25s cubic-bezier(.71,-.46,.29,1.46),background-color .25s cubic-bezier(.71,-.46,.29,1.46);}
+
 .xz-sort__label {display: inline-block;padding-left: 10px;line-height: 19px;font-size: 14px;}
 </style>
